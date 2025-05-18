004090c1f201c13f59043b975a35eecd6d44badd
lsa4153 <lsa4153@naver.com>
2025-05-19 01:02:20 +0900

    sua

--- routes/auth.py
+++ routes/auth.py
@@ -4,7 +4,7 @@ import requests
 import json
 import os
 
-# 구글 OAuth 설정
+# 구글 OAuth 설정..
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
 GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
